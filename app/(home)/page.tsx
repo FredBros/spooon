03b7108b4e36c14c501c5f72e46c1cc0f1208060
Frédéric Brossard <77939827+FredBros@users.noreply.github.com>
2024-02-02 09:37:44 +0100

    feat: add loading skeleton for post page and LikeButton

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,13 +4,13 @@ import Post from "@/src/feature/post/Post";
 import { getLatestPosts } from "@/src/query/post.query";
 import React from "react";
 
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
+// export const revalidate = 0;
+// export const dynamic = "force-dynamic";
+// export const fetchCache = "force-no-store";
 
 export default async function Home() {
-  // const session = await getAuthSession();
-  const posts = await getLatestPosts()
+  const session = await getAuthSession();
+  const posts = await getLatestPosts(session?.user.id)
 
   return <div className="divide-y divide-muted">
     {posts.map((post) => 
--- src/feature/post/LikeButton.tsx
+++ src/feature/post/LikeButton.tsx
@@ -1,15 +1,23 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { Loader } from "@/components/ui/loader";
+import clsx from "clsx";
+import { Heart } from "lucide-react";
+import { useTransition } from "react";
+import { likeAction } from "./like.action";
 
 export default function LikeButton({
-    postId,
-    isLiked,
-}:{
-    postId: string;
-    isLiked: boolean;
+  postId,
+  isLiked,
+}: {
+  postId: string;
+  isLiked: boolean;
 }) {
-  return (
-    <div>LikeButton</div>
-  )
+  const [isPending, startTransition] = useTransition();
+  return <div>
+    <button className={clsx({'rounded-md hover:bg-accent flex gap-1 items-center text-red-500': isLiked,})}
+    onClick={()=>startTransition(()=>likeAction(postId))}>
+      {isPending ? <Loader size={20} /> : <Heart size={20} />}
+    </button>
+  </div>;
 }
